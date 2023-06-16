ae8768a522298324f6f9bffe4e5cc9335f44f918
arthurwingert <arthurwingert067@gmail.com>
2023-06-16 10:56:08 -0300

    alterações no input

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,19 @@
 import styles from "./Home.module.css";
 
 function Home() {
+  function handleClick() {
+    const emailInput = document.getElementById("emailInput");
+    const emailValue = emailInput.value.trim(); // Remove espaços em branco no início e fim
+  
+    if (emailValue === "") {
+      window.alert("Por favor, insira o e-mail");
+    } else {
+      emailInput.value = "";
+      window.alert("E-mail cadastrado com sucesso");
+    }
+  }
+  
+
   return (
     <div className={styles.home_container}>
       <div className={styles.subcontainer}>
@@ -8,40 +21,54 @@ function Home() {
           Bem vindo ao <span>IA System</span>
         </h1>
         <p>Comece seu plano agora mesmo!</p>
+        <div className={styles.input_div}>
+          Fique por dentro das novas notícias
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="                       Digite seu e-mail"
+            id="emailInput"
+          />
+          <div className={styles.alinhamento}>
+            <button className={styles.button} onClick={handleClick}>
+              Enviar
+            </button>
+          </div>
+        </div>
       </div>
+
       <div className={styles.firstmodal}>
-        <span>BÁSICO</span>
+        <span>Basic</span>
         <ul className={styles.containerfirstlist}>
-          <li> APP PARA OS CLIENTES</li>
-          <li> PAGAMENTOS INTERNOS</li>
-          <li> EMISSÃO DE NOTA FISCAL</li>
+          <li> &gt; App para os clientes</li>
+          <li> &gt; Pagamentos internos</li>
+          <li> &gt; Emissão de Nota Fiscal</li>
         </ul>
         <span>R$89,90</span>
       </div>
       <div className={styles.secondmodal}>
-        <span>PADRÃO</span>
+        <span>Standard</span>
         <ul className={styles.containersecondlist}>
-          <li> APP PARA OS CLIENTES</li>
-          <li> PAGAMENTOS INTERNOS</li>
-          <li> EMISSÃO DE NOTA FISCAL</li>
-          <li> GESTÃO DE ESTOQUE</li>
-          <li> ANÁLISE FINANCEIRA</li>
-          <li> EMISSÃO DE NOTA FISCAL;</li>
+          <li> &gt; App para os clientes</li>
+          <li> &gt; Pagamentos internos</li>
+          <li> &gt; Emissão de Nota Fiscal</li>
+          <li> &gt; Gestão de Estoque</li>
+          <li> &gt; Análise Financeira</li>
         </ul>
         <span>R$109,90</span>
       </div>
       <div className={styles.thirdmodal}>
-        <span>PREMIUM</span>
+        <span>Premium</span>
         <ul className={styles.containerthirdlist}>
-          <li> APP PARA OS CLIENTES</li>
-          <li> PAGAMENTOS INTERNOS</li>
-          <li> EMISSÃO DE NOTA FISCAL</li>
-          <li> GESTÃO DE ESTOQUE</li>
-          <li> ANÁLISE FINANCEIRA</li>
-          <li> EMISSÃO DE NOTA FISCAL</li>
-          <li>CRM</li>
-          <li>EMISSÃO DE LAUDO MÉDICO</li>
-          <li>CONTROLE DE AGENDA DOS MÉDICOS E PACIENTES </li>
+          <li> &gt; App para os clientes</li>
+          <li> &gt; Pagamentos internos</li>
+          <li> &gt; Emissão de Nota Fiscal</li>
+          <li> &gt; Gestão de Estoque</li>
+          <li> &gt; Análise Financeira</li>
+          <li> &gt; vEmissão de Nota Fiscal</li>
+          <li> &gt; CRM</li>
+          <li> &gt; Emissão de Laudo Médico</li>
+          <li> &gt; Controle de agenda dos médicos e pacientes </li>
         </ul>
         <span>R$149,90</span>
       </div>
--- src/components/pages/layout/Footer.js
+++ src/components/pages/layout/Footer.js
@@ -1,5 +1,8 @@
 import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import styles from "./Footer.module.css";
+import { MdOutlinePlace } from "react-icons/md";
+import { BsTelephone } from "react-icons/bs";
+import {MdOutlineMail} from 'react-icons/md'
 
 function Footer() {
   return (
@@ -15,6 +18,15 @@ function Footer() {
           <FaWhatsapp />
         </li>
       </ul>
+      <div className={styles.place}>
+        <div></div>
+        <MdOutlinePlace />{" "}
+        <span>Av. Dr. Maurício Cardoso, 1520 - Novo Hamburgo</span>
+        <div className={styles.phone}>
+        <BsTelephone /> 
+        <span>51 3586-2230</span>
+        </div>
+      </div>
       <p className={styles.copy_right}>
         <span>IA System</span> &copy; 2023
       </p>
